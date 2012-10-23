895f23a3632e198c4200b3525fa64225194e0bd8
Jason Jarrett <jason@elegantcode.com>
2012-10-22 23:22:10 -0700

    more presentation updates

---
+++ TimeTracker.ps1
@@ -0,0 +1,34 @@
+﻿
+<#
+
+
+Start-TrackingTime 90
+
+HowsMyTime "some topic"
+
+#>
+
+$timeTracker = @{}
+
+
+function Start-TrackingTime($allottedMinutes) {
+
+    $timeTracker.StartTime = Get-Date
+    $timeTracker.Tick = get-date
+    $timeTracker.ExpectedEndTime = $timeTracker.StartTime.AddMinutes($allottedMinutes)
+
+    
+    Write-Host -ForegroundColor Green "On your marks, get set, GO... Time is being tracked" 
+}
+
+function HowsMyTime($topic){
+    $start = $timeTracker.Tick;
+    $end = $timeTracker.ExpectedEndTime;
+
+    $timeSpent = New-TimeSpan $start (get-date)
+    $timeLeft = New-TimeSpan (get-date) $end
+
+    $timeTracker.Tick = get-date
+
+    ("You've blabbered for {0} minutes about {1} and only have {2} minutes left.") -f ([int]$timeSpent.TotalMinutes), $topic, ([int]$timeLeft.TotalMinutes)
+}
